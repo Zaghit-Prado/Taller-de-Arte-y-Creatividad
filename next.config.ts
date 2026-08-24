64c7b6a8fe41fec58e25b6347824ad236fb7eb21
Zaghit Aaron Prado Martinez <Prado_Martinez@hotmail.com>
2026-08-23 23:42:11 -0500

    Fix: Configurar dominios de imagenes permitidos

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   
   images: {
-    remotePatterns: [
+   remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "**",
       },
     ],
   },
